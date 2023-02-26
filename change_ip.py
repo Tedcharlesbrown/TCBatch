30b76a0d9c75a8d476fc153531306594a1fb2e78
Tedcharlesbrown <52015952+Tedcharlesbrown@users.noreply.github.com>
2023-02-26 14:01:35 -0500

    update

---
+++ change_ip.py
@@ -0,0 +1,152 @@
+import subprocess
+from constants import *
+
+def list_network_adapters() -> list:
+		# Create an empty list to store the interface names
+		interfaces = []
+
+		# Run the "ipconfig" command and save the output
+		output = subprocess.check_output("ipconfig")
+
+		# Decode the output from bytes to a string
+		output = output.decode("utf-8")
+
+		# Split the output into a list of lines
+		for line in output.split("\n"):
+
+			# Check if the line contains the word "adapter"
+			if "adapter" in line:
+
+				# Split the line on the word "adapter" and extract the adapter name
+				adapter = line.split("adapter ")
+				adapter = adapter[1].split(":")
+
+				# Add the adapter name to the list of interfaces
+				interfaces.append(adapter[0])
+
+		# Return the list of interfaces
+		return interfaces
+
+def default_subnet(ip_input: str):
+	# split the IP address by the period character and take the first item in the resulting list
+	octet_one = int(ip_input.split('.')[0])
+	# if the first octet is less than or equal to 127
+	if octet_one <= 127:
+		# return the subnet mask 255.0.0.0
+		return "255.0.0.0"
+
+	# if the first octet is between 128 and 191
+	elif octet_one >= 128 and octet_one <= 191:
+		# return the subnet mask 255.255.0.0
+		return "255.255.0.0"
+
+	# if the first octet is greater than or equal to 192
+	elif octet_one >= 192:
+		# return the subnet mask 255.255.255.0
+		return "255.255.255.0"
+
+
+def parse_subnet(subnet_input: str):
+
+	# convert the input to lowercase to make it case-insensitive
+	subnet_input = subnet_input.lower()
+
+	# try to convert the input to an integer if it cannot be converted, the ValueError exception will be raised
+	try:
+		subnet_index = int(subnet_input)
+	except ValueError:
+		subnet_index = None
+
+	# if the input is in the subnet map, return the corresponding subnet mask
+	if subnet_input in SUBNET_MAP:
+		return SUBNET_MAP[subnet_input]
+
+	# if the input can be converted to an integer and is less than or equal to 33,
+	# return the subnet mask at the specified index in the SUBNET_LIST array
+	elif subnet_index is not None and subnet_index <= 33:
+		return SUBNET_LIST[subnet_index]
+
+	# if the input does not match any of the above conditions,
+	# return the input subnet mask
+	else:
+		return subnet_input
+
+
+def parse_gateway(addresses: list):
+	# split the first address by the period character and store the resulting list
+	ip = addresses[0].split('.')
+
+	# store the third address as the gateway
+	gateway = addresses[2]
+
+	# if the length of the gateway address is less than or equal to 12
+	if len(gateway) <= 12:
+		# if the gateway address does not contain a period,
+		# build the gateway address using the first three octets of the first address
+		if '.' not in gateway:
+			gateway = f"{ip[0]}.{ip[1]}.{ip[2]}.{gateway}"
+
+		# if the gateway address contains one or more periods,
+		# split the gateway address by the period character and store the resulting list
+		else:
+			gateway = gateway.split('.')
+
+			# if the gateway address has two octets,
+			# build the gateway address using the first three octets of the first address and the second octet of the gateway address
+			if len(gateway) == 2:
+				gateway = f"{ip[0]}.{ip[1]}.{ip[2]}.{gateway[1].replace('.','')}"
+
+			# if the gateway address has three octets,
+			# build the gateway address using the first and second octets of the first address and the second and third octets of the gateway address
+			elif len(gateway) == 3:
+				gateway = f"{ip[0]}.{ip[1]}.{gateway[1].replace('.','')}.{gateway[2].replace('.','')}"
+
+			# if the gateway address has four octets,
+			# build the gateway address using the first octet of the first address and the second, third, and fourth octets of the gateway address
+			elif len(gateway) == 4:
+				gateway = f"{ip[0]}.{gateway[1].replace('.','')}.{gateway[2].replace('.','')}.{gateway[3].replace('.','')}"
+
+	# return the gateway address
+	return gateway
+
+
+def parse_dns( dns_input: str, is_primary: bool):
+	dns = ""
+	if len(dns_input) == 0:
+		if is_primary:
+			dns = "1.1.1.1"
+		else:
+			dns = "8.8.8.8"
+	else:
+		dns = dns_input
+
+	return dns
+
+
+def set_network_adapter(interface: str, addresses: list, primary_dns, secondary_dns):
+	cmd = ""
+
+	if addresses[0].lower() == "dhcp":
+		cmd = "netsh interface ip set address \"" + interface + "\" dhcp"
+
+	elif len(addresses) == 1:
+		cmd = "netsh interface ip set address \"" + interface + "\" static " + addresses[0] + " " + default_subnet(addresses[0])
+
+	elif len(addresses) == 2:
+		cmd = "netsh interface ip set address \"" + interface + "\" static " + addresses[0] + " " + parse_subnet(addresses[1])
+
+	elif len(addresses) >= 3:
+		dns_clear = f"Get-NetAdapterBinding -ComponentID 'ms_tcpip6' -InterfaceAlias '{interface}' | " \
+             "where-object {$_.DisplayName -eq 'Internet Protocol Version 4 (TCP/IPv4)'} | " \
+             "Set-DnsClientServerAddress -ResetServerAddresses"
+		
+		subprocess.run(["powershell", "-Command", dns_clear])
+		
+		cmd = "netsh interface ip set address \"" + interface + "\" static " + addresses[0] + " " + parse_subnet(addresses[1]) + " " + parse_gateway(addresses)
+		cmd += "\r"
+		cmd += "netsh interface ip set dns \"" + interface + "\" static " + parse_dns(primary_dns, True)
+		cmd += "\r"
+		cmd += "netsh interface ip add dns \"" + interface + "\" " + parse_dns(secondary_dns, False) + " index=2"
+		
+
+	subprocess.call(["powershell.exe", cmd])
--- main.py
+++ main.py
@@ -11,13 +11,16 @@ import ipaddress
 import pyuac
 
 import questionary
+from questionary import Style
+from questionary import Validator, ValidationError, prompt
 
 
 import winshell
 
 from classes import MENU
 from menu_change_name import *
-from menu_change_ip import *
+from change_ip import list_network_adapters
+from change_ip import set_network_adapter
 
 from menu_download_software import *
 
@@ -26,6 +29,41 @@ from menu_symlink import *
 from menu_restart import *
 
 from constants import *
+
+custom_style = Style([
+    ('qmark', 'fg:#673ab7 bold'),       # token in front of the question
+    ('question', 'bold'),               # question text
+    # ('answer', 'fg:#f44336 bold'),      # submitted answer text behind the question
+    ('answer', 'fg:#039300 bold'),      # submitted answer text behind the question
+    ('pointer', 'fg:#673ab7 bold'),     # pointer used in select and checkbox prompts
+    # ('highlighted', 'fg:#673ab7 bold'), # pointed-at choice in select and checkbox prompts
+    ('highlighted', 'fg:#7000A9 bold'), # pointed-at choice in select and checkbox prompts
+    ('selected', 'fg:#cc5454'),         # style for a selected item of a checkbox
+    ('separator', 'fg:#cc5454'),        # separator in lists
+    ('instruction', ''),                # user instructions for select, rawselect, checkbox
+    ('text', ''),                       # plain text
+    ('disabled', 'fg:#858585 italic')   # disabled choices for select and checkbox prompts
+])
+	
+def ask_select(message: str, choices: list, return_index: bool):
+
+	answer = questionary.select(
+    f"{message}\n",
+    qmark="",
+    instruction=" ",
+    style=custom_style,
+    choices=choices,
+	).ask()
+
+	if return_index:
+		for i, choice in enumerate(choices):
+			if answer == choice:
+				return(i)
+	else:
+		return answer
+
+def ask_text(message: str):
+	return questionary.text(message,qmark="",style=custom_style).ask()
 
 
 # ---------------------------------------------------------------------------- #
@@ -41,31 +79,80 @@ def folder_application_init():
 #                                   MAIN MENU                                  #
 # ---------------------------------------------------------------------------- #
 
-def menu():
-	m_main = MENU("MAIN MENU", APP_NAME)
-	m_name = MENU_name("Change Computer Name", ASCII_COMPUTER_NAME)
-	m_ip = MENU_ip("Change IP Addresses", ASCII_IP_ADDRESS)
-	m_download = MENU_download("Download Software", ASCII_DOWNLOAD)
-	m_software = MENU_software("Install Software", ASCII_SOFTWARE)
-	m_symlink = MENU_symlink("Create Symlink Folder", ASCII_SYMLINK)
-	m_restart = MENU_restart("Restart Computer", ASCII_RESTART)
+def menu_main():
+	choices = ["Change Computer Name", "Change IP Addresses", "Download Software", "Install Software", "Create Symlink Folder", "Restart Computer"]
 
-	m_main.add_option(m_name)
-	m_main.add_option(m_ip)
-	m_main.add_option(m_download)
-	m_main.add_option(m_software)
-	m_main.add_option(m_symlink)
-	m_main.add_option(m_restart)
+	match ask_select(APP_NAME,choices,True):
+		case 0:
+			menu_change_computer_name()
+		case 1:
+			menu_change_ip_address()
+		case 2:
+			menu_download_software()
 
-	m_main.enter()
+
+def menu_change_computer_name():
+	user_input = questionary.text(
+	f"{ASCII_COMPUTER_NAME}\nTYPE NEW NAME AND PRESS 'ENTER', OR PRESS 'ENTER' TO CANCEL\nCURRENT COMPUTER NAME = " + "'" + platform.node() + "'\n\n",
+	qmark="",
+	style=custom_style
+	).ask()
+
+	if user_input == "":
+			# print("GOING BACK TO MAIN MENU")
+			questionary.print(DIVIDER, style="bold")	
+			menu_main()
+	else:
+		if questionary.confirm(f"\nCHANGE NAME TO {user_input}?",qmark="",style=custom_style).ask():
+			print()
+			questionary.print(DIVIDER, style="bold")
+			questionary.print("CHANGING NAME TO: " + user_input, style="bold")
+			# subprocess.call(['powershell.exe', "Rename-Computer -NewName " + user_input])
+			questionary.print(DIVIDER, style="bold")
+			menu_main()
+		else:
+			print()
+			questionary.print(DIVIDER, style="bold")	
+			menu_main()
+
+def menu_change_ip_address():
+
+	interface = ask_select(ASCII_IP_ADDRESS,list_network_adapters(),False)
+
+	return_string = []
+	primary_dns = ""
+	secondary_dns = ""
+	ip_adddress = ask_text("IP ADDRESS:")
+	if ip_adddress:
+		return_string.append(ip_adddress)
+		subnet = ask_text("SUBNET:")
+
+		if subnet:
+			return_string.append(subnet)
+			gateway = ask_text("GATEWAY:")
+
+			if gateway:
+				return_string.append(gateway)
+				primary_dns = ask_text("PRIMARY DNS:")
+
+				if primary_dns:
+					secondary_dns = ask_text("SECONDARY DNS:")
+
+		set_network_adapter(interface, return_string, primary_dns, secondary_dns)
+
+	menu_main()
+
+
+def menu_download_software():
+	pass
 
 # ---------------------------------------------------------------------------- #
 #                                     MAIN                                     #
 # ---------------------------------------------------------------------------- #
 
 def main():
-	# folder_application_init()
-	# menu()
+	folder_application_init()
+	menu_main()
 
 
 if __name__ == "__main__":
